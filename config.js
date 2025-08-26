2d7b13f0d32558cf9afa023941c24dbf71e1ec68
ParvulosHosting <savaleksic10@gmail.com>
2025-08-26 22:43:03 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// Configuración de usuarios - Este archivo se puede reemplazar en diferentes entornos
+const USER_CONFIG = {
+  'luis.tejeda': {
+    password: 'PorciusPork123',
+    role: 'user'
+  },
+  'sava.aleksic': {
+    password: 'Pork12345$',
+    role: 'admin'
+  }
+};
